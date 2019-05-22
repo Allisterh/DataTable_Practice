df0abd2172c053859f4db05391cba5189d2469e8
BriVandenAkker <47199217+BriVandenAkker@users.noreply.github.com>
2019-05-22 16:32:01 -0400

    Update adec7910_HW3.R

--- adec7910_HW3.R
+++ adec7910_HW3.R
@@ -20,7 +20,7 @@
 
   
 ##--------------------------------------------------------------------
-##----- Q1 - import data and rename variables
+##----- import data and rename variables
 ##--------------------------------------------------------------------
   
   # Import data as data.table
@@ -39,10 +39,10 @@
   rm(i, new.var, old.var, vars)
 
 ##--------------------------------------------------------------------
-##----- Q2 - store registry
+##----- store registry
 ##--------------------------------------------------------------------
   
-  # Q2.1 - Filter out unique store records
+  # Filter out unique store records
   #Locates unique combinations of specified variables
   stores <- unique(sales[, .(store.number, store.name, address, store.location, city, zip.code, county)]) 
   #1925 unique store numbers, therefore we have duplicates to address
@@ -50,26 +50,26 @@
   #Drop these variables (except store.number) from sales
   sales[, c("store.name", "store.location", "city", "zip.code", "county", "address") := NULL]
   
-  # Q2.2 - Filter out store GPS coordinates from location variable
+  # Filter out store GPS coordinates from location variable
   #Isolate coordinates
   stores[, c("location") := tstrsplit(store.location, "[()]", keep = 2)]
   #Specify latitude/longitude
   stores[, c("store.latitude", "store.longitude") := tstrsplit(location, ",")]
   
-  # Q2.3 - Drop location variable
+  # Drop location variable
   stores[, c("store.location", "location") := NULL]
   
-  # Q2.4 - Average GPS coordinates for stores
+  # Average GPS coordinates for stores
   stores[, ':='(store.longitude = as.numeric(store.longitude), 
                 store.latitude = as.numeric(store.latitude))
         ][, ':='(store.latitude = mean(store.latitude),
                  store.longitude = mean(store.longitude)),
           by = store.number]
   
-  # Q2.5 - Removing duplicates
+  # Removing duplicates
   stores <- unique(stores)
   
-  # Q2.6 - Fix address, city and county names
+  # Fix address, city and county names
   stores <- stores[, c("address","city","county") := lapply(.SD, str_to_title), .SDcols = c("address","city","county") #Homogenize city, county, and address variables
                   ][, address := gsub("[.,]", "", address) #Remove periods and commas in address
                   ][, store.name := gsub("\\#.*","", store.name) #Remove pound numbers in store name
@@ -89,11 +89,10 @@
                   ][grepl("East", address, ignore.case = TRUE), address := gsub("East","E", address)
                   ][grepl("North", address, ignore.case = TRUE), address := gsub("North","N", address)]
   
-  # Q2.7 - Remove duplicates
-  #Dropping ten too many here...
+  # Remove duplicates
   stores <- unique(stores)
   
-  # Q2.8 - add zipcode/city/county matches
+  # add zipcode/city/county matches
   geo <- fread("iowa.geographies.csv")
   #Generate new variable
   geo[, match := 1]
@@ -113,38 +112,37 @@
   stores[,.N, match == 0]
   rm(geo)
   
-  # Q2.9 - remove duplicates with match = 0
+  # remove duplicates with match = 0
   #Locate duplicates by row number
   stores <- stores[, ':='(duplicate = (duplicated(stores[,store.number]) | duplicated(stores[,store.number], fromLast = TRUE)))]
   #Locate duplicates with both 0/1 match
   stores <- stores[,ingeo := max(match), by = store.number]
   #Remove the duplicates with match = 0 which have both 0/1 match markers
   stores <- stores[ingeo == 0 | !(match == 0 & duplicate == TRUE)]
-  #Dropping one too many here...
   stores[,.N, match == 0]
   stores[, c("match", "duplicate", "ingeo") := NULL]
   
   
 ##--------------------------------------------------------------------
-##----- Q3 - cleaning sales data
+##----- cleaning sales data
 ##--------------------------------------------------------------------
   
-  # Q3.1 - Convert sales and prices to proper format
+  # Convert sales and prices to proper format
   sales <- sales[, ':='(state.bottle.retail = as.numeric(gsub("[$]", "", state.bottle.retail)),
                          sale.dollars = as.numeric(gsub("[$]", "", sale.dollars)))]
                                                           
-  # Q3.2 - Create subcategory variable
+  # Create subcategory variable
   sales <- sales[, ':='(subcategory = str_to_title(category.name),
                         category.name = NULL)]
   
-  # Q3.3 - Create proper category variable
+  # Create proper category variable
   sales <- sales[, category := word(subcategory, -1)
                 ][, category := gsub("s", "", category)
                 ][, category := gsub("ie", "y", category)]
   
   
 ##--------------------------------------------------------------------
-##----- Q4 - export cleaned data
+##----- export cleaned data
 ##--------------------------------------------------------------------
   
   fwrite(sales, file = "sales.csv")
